4ac4e282de0ac49cd06c2f6c1979a472778eef87
Robot-V2 <80040431+Robot-V2@users.noreply.github.com>
2021-05-04 02:07:03 +0000

    Update 0-subs.py

--- 0x06-api_advanced/0-subs.py
+++ 0x06-api_advanced/0-subs.py
@@ -6,10 +6,7 @@ def number_of_subscribers(subreddit):
 
     import requests
 
-    resInf = requests.get("https://www.reddit.com/r/{}/about.json"
-                            .format(subreddit),
-                            headers={"User-Agent": "My-User-Agent"},
-                            allow_redirects=False)
+    resInf = requests.get("https://www.reddit.com/r/{}/about.json".format(subreddit),headers={"User-Agent": "My-User-Agent"},allow_redirects=False)
     if resInf.status_code >= 300:
         return 0
 
